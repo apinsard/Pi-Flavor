887243ffc8c34254de2c21c39348301a3a2b6a9d
Antoine Pinsard <antoine.pinsard@gmail.com>
2014-11-11 15:01:49 +0100

    Started refactor and comment

--- src/gpio.s
+++ src/gpio.s
@@ -1,47 +1,77 @@
 @ Distributed under the terms of the GNU General Public License v2
+.globl GPIO_ADDR
+.globl GPIO_NB_PINS
+.globl GPIO_NB_FUNCS
+.globl GPIO_BLOCK_SIZE
+.globl GPIO_PINS_PER_BLOCK
 .globl GetGpioAddress
 .globl SetGpioFunction
 .globl SetGpio
 
-@ Load the GPIO Address
-@ r0 <- GPIO Address
+.equiv GPIO_ADDR,       0x20200000
+.equiv GPIO_NB_PINS,    54
+.equiv GPIO_NB_FUNCS,   8
+.equiv GPIO_BLOCK_SIZE, 4
+.equiv GPIO_PINS_PER_BLOCK, 10
+
+@
+@ Load the GPIO Address in memory.
+@ => r0: GPIO Address
+@ == r[1..3]
 GetGpioAddress:
-  ldr r0, =0x20200000
+  ldr r0, =GPIO_ADDR
   mov pc, lr
 
-@ Apply a GPIO function to a pin
-@ r0: GPIO pin [0..53]
-@ r1: Function to apply [0..7]
+@
+@ Apply a GPIO function to a pin.
+@ ~> r0: The offset of the 3 bits of the function pin in the block
+@ ~> r1: The pin function at the right offset in the block (content of the block)
+@ ~> r2: The address of the block containing the given pin function.
+@ == r3
 SetGpioFunction:
-  cmp   r0, #53
-  cmpls r1, #7
-  movhi pc, lr @ Break if pre-cond not satisfied
+  pinNum  .req r0 @ <- GPIO pin [0, GPIO_NB_PINS[
+  pinFunc .req r1 @ <- Function [0, GPIO_NB_FUNCS[
 
-  push {lr}
-  mov  r2, r0
-  bl   GetGpioAddress
+  @ Check pre-conditions and break if not satisfied.
+  cmp   pinNum, #[GPIO_NB_PINS-1]
+  cmpls pinNum, #[GPIO_NB_FUNCS-1]
+  movhi pc, lr @ <Return
 
-  functionLoop$:
-    cmp   r2, #9
-    subhi r2, #10
-    addhi r0, #4
-    bhi   functionLoop$
+  ldr r2, =GPIO_ADDR
+  funcPinAddr .req r2
 
-  add r2, r2, lsl #1
-  lsl r1, r2
-  str r1, [r0]
-  pop {pc}
+  @ Calculate the function pin address (which block of 4 bytes?) in funcPinAddr
+  @ and which set of 3 bits in pinNum.
+  funcCalcLoop$:
+    cmp   pinNum, #[GPIO_PINS_PER_BLOCK-1]
+    subhi pinNum, #GPIO_PINS_PER_BLOCK
+    addhi funcPinAddr, #GPIO_BLOCK_SIZE
+  bhi funcCalcLoop$
+  
+  @ Multiply pinNum by 3 to get the offset of the given set of 3 bits.
+  add pinNum, pinNum, lsl #1
+  .unreq pinNum
+  pinOffset .req r0
+
+  @ Set the pin function
+  lsl pinFunc, pinOffset
+  str pinFunc, [funcPinAddr]
+  .unreq pinOffset
+  .unreq pinFunc
+  .unreq funcPinAddr
+
+  mov pc, lr @ <Return
 
 @ Turn on/off a GPIO pin.
-@ r0: GPIO pin [0..53]
+@ r0: GPIO pin [0, GPIO_NB_PINS-1[
 @ r1: Turn off? (0) or turn on? (any other value)
 SetGpio:
   pinNum .req r0
   pinVal .req r1
 
-  cmp   pinNum, #53
+  cmp   pinNum, #[GPIO_NB_PINS-1]
   movhi pc, lr @ Break if pre-cond not satisfied
-  
+
   push {lr}
   mov  r2, pinNum
   .unreq pinNum
