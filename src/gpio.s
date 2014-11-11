ebf3fe9f4af7bd41f193afe7cab45c45a4ea0740
Antoine Pinsard <antoine.pinsard@gmail.com>
2014-11-11 22:46:26 +0100

    Refactor gpio.s

--- src/gpio.s
+++ src/gpio.s
@@ -4,15 +4,21 @@
 .globl GPIO_NB_FUNCS
 .globl GPIO_BLOCK_SIZE
 .globl GPIO_PINS_PER_BLOCK
+.globl GPIO_PIN_TURN_ON
+.globl GPIO_PIN_TURN_OFF
+.globl GPIO_PIN_INPUT
 .globl GetGpioAddress
 .globl SetGpioFunction
 .globl SetGpio
 
-.equiv GPIO_ADDR,       0x20200000
-.equiv GPIO_NB_PINS,    54
-.equiv GPIO_NB_FUNCS,   8
-.equiv GPIO_BLOCK_SIZE, 4
+.equiv GPIO_ADDR        , 0x20200000
+.equiv GPIO_NB_PINS     , 54
+.equiv GPIO_NB_FUNCS    , 8
+.equiv GPIO_BLOCK_SIZE  , 4
 .equiv GPIO_PINS_PER_BLOCK, 10
+.equiv GPIO_PIN_TURN_ON , 28
+.equiv GPIO_PIN_TURN_OFF, 40
+.equiv GPIO_PIN_INPUT   , 52
 
 @
 @ Load the GPIO Address in memory.
@@ -37,8 +43,8 @@ SetGpioFunction:
   cmpls pinNum, #[GPIO_NB_FUNCS-1]
   movhi pc, lr @ <Return
 
-  ldr r2, =GPIO_ADDR
   funcPinAddr .req r2
+  ldr funcPinAddr, =GPIO_ADDR
 
   @ Calculate the function pin address (which block of 4 bytes?) in funcPinAddr
   @ and which set of 3 bits in pinNum.
@@ -78,40 +84,42 @@ SetGpioFunction:
 
   mov pc, lr @ <Return
 
+@
 @ Turn on/off a GPIO pin.
-@ r0: GPIO pin [0, GPIO_NB_PINS-1[
-@ r1: Turn off? (0) or turn on? (any other value)
+@
 SetGpio:
-  pinNum .req r0
-  pinVal .req r1
+  pinNum .req r0 @ <- GPIO pin [0, GPIO_NB_PINS[
+  pinVal .req r1 @ <- 0: Turn OFF ; Else: Turn ON
 
+  @ Check pre-conditions and break if not satisfied.
   cmp   pinNum, #[GPIO_NB_PINS-1]
-  movhi pc, lr @ Break if pre-cond not satisfied
+  movhi pc, lr @ <Return
 
-  push {lr}
-  mov  r2, pinNum
-  .unreq pinNum
-  pinNum .req r2
-  bl GetGpioAddress
-  gpioAddr .req r0
+  gpioPinAddr .req r2
+  ldr gpioPinAddr, =GPIO_ADDR
 
+  @ Divide the GPIO pin number by 32 to get in which of the two sets of 4 bytes
+  @ it is (0 or 1). And then multiply by 4 to get the address.
   pinBank .req r3
   lsr pinBank, pinNum, #5
-  lsl pinBank, #2
-  add gpioAddr, pinBank
+  add gpioPinAddr, pinBank, lsl #2
   .unreq pinBank
 
+  @ The bit to set at the given address is the nth bit where n is the remainder
+  @ of the previous division by 32.
   and pinNum, #31
   setBit .req r3
   mov setBit, #1
   lsl setBit, pinNum
   .unreq pinNum
 
+  @ Turn ON or OFF within pinVal
   teq pinVal, #0
   .unreq pinVal
-  streq setBit,[gpioAddr,#40]
-  strne setBit,[gpioAddr,#28]
+  streq setBit,[gpioPinAddr,#GPIO_PIN_TURN_OFF]
+  strne setBit,[gpioPinAddr,#GPIO_PIN_TURN_ON]
   .unreq setBit
-  .unreq gpioAddr
-  pop {pc}
+  .unreq gpioPinAddr
+
+  mov pc, lr @ <Return
 
